7f5afd7260428d7c7ae76200daa1e603d037fed9
mallius <mallius@qq.com>
2013-11-05 14:17:01 +0800

    sample

---
+++ sample/gdb-sample.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int nGlobalVar = 0;
+
+
+
+
+
+
+int tempFunction(int a, int b)
+{
+	printf("tempFunction is called, a = %d, b = %d\n", a, b);
+	return(a + b);
+}
+
+int main(void)
+{
+	int n;
+	n = 1;
+	n++;
+	n--;
+
+	nGlobalVar += 100;
+	nGlobalVar -= 12;
+
+	printf("n = %d, nGlobalVar = %d\n", n, nGlobalVar);
+
+	n = tempFunction(1, 2);
+	printf("n = %d",n);
+
+	return 0;
+}
